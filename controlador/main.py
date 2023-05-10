5db743f70c3a096ab1ec1acb8a1a36c408647c60
Leonardo Rossi Leao <leonardo.leao@cnpem.br>
2023-05-10 14:36:30 -0300

    inicio da interface gráfica

---
+++ controlador/main.py
@@ -0,0 +1,16 @@
+from PyQt5.QtWidgets import *
+from PyQt5.uic import loadUi
+from PyQt5.QtGui import *
+from PyQt5.QtCore import *
+
+class DataAnalyzer(QMainWindow):
+    
+    def __init__(self):
+        QMainWindow.__init__(self)
+        loadUi("./gui.ui", self)
+        self.setWindowTitle("Controlador do Robô")
+
+app = QApplication([])
+window = DataAnalyzer()
+window.show()
+app.exec_()
